12ad08f57372a7de62d5826c6d6a2c81435f5c65
profbreno <brenod123@gmail.com>
2022-04-29 13:20:43 -0300

    Desafio1

--- src/Desafio1/Q1/Desafio1.java
+++ src/Desafio1/Q1/Desafio1.java
@@ -1,4 +1,31 @@
 package Desafio1.Q1;
 
+//Scanner.java está dentro da seguinte pasta java/util/
+import java.util.Scanner;
+
 public class Desafio1 {
+    public static void main(String[] args) {
+        double n1, n2, media;
+        int x = 1;
+        while(x==1) {
+            Scanner leitor = new Scanner(System.in);
+            do {
+                System.out.println("Digite nota 1");
+                n1 = leitor.nextDouble();
+            } while (n1 < 0 || n1 > 10);
+            do {
+                System.out.println("Digite nota 2");
+                n2 = leitor.nextDouble();
+            } while (n2 < 0 || n2 > 10);
+
+            //Cálculo da média
+            media = (n1 + n2) / 2;
+            System.out.println("A média é " + media);
+
+            do {
+                System.out.println("novo calculo (1-sim 2-nao)");
+                x = leitor.nextInt();
+            } while(x != 1 && x != 2);
+        }
+    }
 }
